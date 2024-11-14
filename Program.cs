befacd1ffb670c647cdff180b3cda35699a0fc14
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-14 11:21:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignments_2._3._2
+{
+    using System;
+
+    class Tip
+    {
+        static void Main()
+        {
+            Console.Write("Enter the total bill amount: ");
+            decimal billTotal = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Enter the tip %: ");
+            decimal tipPercentage = decimal.Parse(Console.ReadLine());
+
+            decimal tipAmount = billTotal * (tipPercentage / 100);
+
+            decimal grandTotal = billTotal + tipAmount;
+
+            Console.WriteLine($"Bill Total: {billTotal:C}");
+            Console.WriteLine($"Tip Amount: {tipAmount:C}");
+            Console.WriteLine($"Grand Total: {grandTotal:C}");
+        }
+    }
+
+}
